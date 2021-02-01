de684e17078ad02723075f06f584d738375aa0ef
Wesley Flôres <wesleyfloresterres@gmail.com>
2021-01-31 23:28:33 -0300

    App.jsx

---
+++ frontend/src/main/App/App.jsx
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
+
+import React from 'react';
+
+import Routes from '../../Routes';
+
+function App() {
+    return (
+        <Routes />
+    );
+}
+
+export default App;
