ffedb8503c20d660322736f09781f34f5e3d5f0d
Wesley Flôres <wesleyfloresterres@gmail.com>
2021-01-31 23:31:41 -0300

    Footer.jsx

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './Footer.css';
+
+function Footer() {
+    return (
+        <footer className="footer">
+            <span>
+                Desenvolvido com <i className="fa fa-heart text-danger" /> por <strong>Wesley Flôres</strong>
+            </span>
+        </footer>
+    );
+}
+
+export default Footer;
