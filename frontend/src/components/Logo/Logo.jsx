c681660d1da12916d7f26093b89d967ede5c2e55
Wesley Flôres <wesleyfloresterres@gmail.com>
2021-01-31 23:36:28 -0300

    Logo.jsx

--- frontend/src/components/Logo/Logo.jsx
+++ frontend/src/components/Logo/Logo.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
 import './Logo.css';
 
 import LogoImage from '../../assets/Logo.png';
@@ -7,9 +9,9 @@ import LogoImage from '../../assets/Logo.png';
 function Logo() {
     return (
         <aside className="logo">
-            <a href="/" className="logo">
+            <Link to="/" className="logo">
                 <img src={LogoImage} alt="Logo" />
-            </a>
+            </Link>
         </aside>
     );
 }
