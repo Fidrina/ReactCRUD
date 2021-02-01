f4df2d0db52bac535e81058036d122f2b5831fa8
Wesley Flôres <wesleyfloresterres@gmail.com>
2021-01-31 22:29:18 -0300

    Nav.jsx

---
+++ frontend/src/components/Nav/Nav.jsx
@@ -0,0 +1,30 @@
+import './Nav.css';
+
+import React from 'react';
+
+import { Link } from 'react-router-dom';
+
+const Navigation = [
+    { icon: `fa fa-home`, title: `Home`, to: `/` },
+    { icon: `fa fa-users`, title: `User`, to: `/users` }
+];
+
+function Nav() {
+    return (
+        <aside className="menu-area">
+            <nav className="menu">
+                {
+                    Navigation.map(function (el, key) {
+                        return (
+                            <Link to={el.to}>
+                                <i className={el.icon}></i> {el.title}
+                            </Link>
+                        )
+                    })
+                }
+            </nav>
+        </aside>
+    );
+}
+
+export default Nav;
